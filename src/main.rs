a7bd8083bc768eb7732b6ea9fa57b08d0d049f54
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-04-04 19:47:05 +0200

    Working on refactoring code

--- src/main.rs
+++ src/main.rs
@@ -5,128 +5,12 @@ use std::time::Instant;
 
 use glfw::{Action, Context, Key};
 use glam::f32::{ Mat4, Vec2, Vec3 };
-use wgpu::util::DeviceExt;
+
+use render::{ GpuDevice, Mesh, Vertex, UniformBuffer };
 
 extern crate glfw;
 
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct Vertex {
-    position: [f32; 3],
-    color: [f32; 3],
-}
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct UniformBuffer {
-    projection_matrix: [f32; 4 * 4],
-    view_matrix: [f32; 4 * 4],
-    model_matrix: [f32; 4 * 4],
-}
-
-impl UniformBuffer {
-    fn new(projection_matrix: Mat4, view_matrix: Mat4, model_matrix: Mat4) -> Self {
-        let mut result = Self {
-            projection_matrix: [0.0; 4 * 4],
-            view_matrix: [0.0; 4 * 4],
-            model_matrix: [0.0; 4 * 4],
-        };
-
-        result.update(projection_matrix, view_matrix, model_matrix);
-
-        result
-    }
-
-    fn update(&mut self, projection_matrix: Mat4, view_matrix: Mat4, model_matrix: Mat4) {
-        projection_matrix.write_cols_to_slice(&mut self.projection_matrix);
-        view_matrix.write_cols_to_slice(&mut self.view_matrix);
-        model_matrix.write_cols_to_slice(&mut self.model_matrix);
-    }
-
-    fn update_view(&mut self, view: Mat4) {
-        view.write_cols_to_slice(&mut self.view_matrix);
-    }
-
-    fn update_model(&mut self, model: Mat4) {
-        model.write_cols_to_slice(&mut self.model_matrix);
-    }
-}
-
-struct Texture {
-    texture: wgpu::Texture,
-    view: wgpu::TextureView,
-}
-
-
-impl Texture {
-    const DEPTH_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth32Float;
-
-    fn create_depth_texture(device: &wgpu::Device, width: u32, height: u32, label: &str)
-        -> Self
-    {
-        let size = wgpu::Extent3d {
-            width,
-            height,
-            depth_or_array_layers: 1,
-        };
-
-        let desc = wgpu::TextureDescriptor {
-            label: Some(label),
-            size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: Self::DEPTH_FORMAT,
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT |
-                    wgpu::TextureUsages::TEXTURE_BINDING,
-        };
-
-        let texture = device.create_texture(&desc);
-
-        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-        Self { texture, view }
-    }
-}
-
-struct Mesh {
-    vertex_buffer: wgpu::Buffer,
-    index_buffer: wgpu::Buffer,
-    index_count: u32,
-}
-
-impl Mesh {
-    fn from_data(gpu_device: &GpuDevice,
-                 vertex_buffer: &Vec<Vertex>,
-                 index_buffer: &Vec<u32>)
-        -> Self
-    {
-        let index_count = index_buffer.len();
-
-        let vertex_buffer = gpu_device.device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Vertex Buffer"),
-                contents: bytemuck::cast_slice(vertex_buffer),
-                usage: wgpu::BufferUsages::VERTEX,
-            }
-        );
-
-        let index_buffer = gpu_device.device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Index Buffer"),
-                contents: bytemuck::cast_slice(index_buffer),
-                usage: wgpu::BufferUsages::INDEX,
-            }
-        );
-
-
-        Self {
-            vertex_buffer,
-            index_buffer,
-            index_count: index_count.try_into().unwrap(),
-        }
-    }
-}
+mod render;
 
 struct Camera2D {
     pos: Vec2,
@@ -203,190 +87,8 @@ impl GameState {
     }
 }
 
-impl Vertex {
-    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-        wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
-                }
-            ]
-        }
-    }
-}
-
 struct Map {
     meshes: Vec<Mesh>,
-}
-
-struct GpuDevice {
-    surface: wgpu::Surface,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    config: wgpu::SurfaceConfiguration,
-
-    width: u32,
-    height: u32,
-
-    depth_texture: Texture,
-    render_pipeline: wgpu::RenderPipeline,
-
-    uniform_buffer: wgpu::Buffer,
-    uniform_buffer_bind_group: wgpu::BindGroup,
-}
-
-impl GpuDevice {
-    async fn new(window: &glfw::Window,
-                 width: u32, height: u32,
-                 initial_uniform_buffer: UniformBuffer)
-        -> Option<Self>
-    {
-        let instance = wgpu::Instance::new(wgpu::Backends::all());
-        let surface = unsafe { instance.create_surface(window) };
-        let adapter = instance.request_adapter(
-            &wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
-            },
-        ).await.unwrap();
-        println!("Adapter: {:#?}", adapter);
-
-        let (device, queue) = adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                features: wgpu::Features::POLYGON_MODE_LINE, // wgpu::Features::default(),
-                limits: wgpu::Limits::default(),
-                label: None,
-            },
-            None, // Trace path
-        ).await.unwrap();
-
-        let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface.get_preferred_format(&adapter).unwrap(),
-            width,
-            height,
-            present_mode: wgpu::PresentMode::Fifo,
-        };
-
-        surface.configure(&device, &config);
-
-        let shader = device.create_shader_module(&wgpu::include_wgsl!("shader.wgsl"));
-
-        let uniform_buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Uniform Buffer"),
-                contents: bytemuck::cast_slice(&[initial_uniform_buffer]),
-                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-            }
-        );
-
-        let uniform_buffer_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            entries: &[
-                wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::VERTEX,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }
-            ],
-            label: Some("uniform_buffer_bind_group_layout"),
-        });
-
-        let uniform_buffer_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &uniform_buffer_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: uniform_buffer.as_entire_binding(),
-                }
-            ],
-            label: Some("uniform_buffer_bind_group"),
-        });
-
-        let depth_texture = Texture::create_depth_texture(&device, width, height, "Depth Texture");
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[&uniform_buffer_bind_group_layout],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main", // 1.
-                buffers: &[Vertex::desc()], // 2.
-            },
-            fragment: Some(wgpu::FragmentState { // 3.
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[wgpu::ColorTargetState { // 4.
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                }],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Cw, // 2.
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: Some(wgpu::DepthStencilState {
-                format: Texture::DEPTH_FORMAT,
-                depth_write_enabled: true,
-                depth_compare: wgpu::CompareFunction::Less,
-                stencil: wgpu::StencilState::default(),
-                bias: wgpu::DepthBiasState::default(),
-            }),
-            multisample: wgpu::MultisampleState {
-                count: 1, // 2.
-                mask: !0, // 3.
-                alpha_to_coverage_enabled: false, // 4.
-            },
-            multiview: None, // 5.
-        });
-
-        Some(Self {
-            surface,
-            device,
-            queue,
-            config,
-
-            width,
-            height,
-
-            depth_texture,
-
-            render_pipeline,
-
-            uniform_buffer,
-            uniform_buffer_bind_group,
-        })
-    }
 }
 
 fn load_map<P>(filename: P, gpu_device: &GpuDevice) -> Option<Map>
@@ -404,6 +106,7 @@ fn load_map<P>(filename: P, gpu_device: &GpuDevice) -> Option<Map>
 
     for sector in &mime_map.sectors {
         let mut vertex_buffer = Vec::new();
+
         for v in &sector.vertex_buffer {
             vertex_buffer.push(Vertex {
                 position: [v.x, v.y, v.z],
@@ -459,7 +162,7 @@ fn main() {
                                                        window_height.try_into().unwrap(),
                                                        uniform_buffer)).unwrap();
 
-    let mut map = load_map("/Users/patrikrosenstrom/wad_reader/map.mup",
+    let mut map = load_map("/Users/patrikrosenstrom/doom1.mup",
                            &gpu_device)
         .expect("Failed to load map");
 
